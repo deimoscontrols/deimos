c55275cfce11016e68560d169556554398741ef2
James Logan <jlogan03@gmail.com>
2025-10-13 21:15:21 -0400

    use clamp

--- software/deimos/src/calc/butter.rs
+++ software/deimos/src/calc/butter.rs
@@ -75,9 +75,8 @@ impl Calc for Butter2 {
         self.output_index = output_range.clone().next().unwrap();
 
         let sample_rate_hz = 1e9f64 / f64::from(ctx.dt_ns);
-        let cutoff_ratio = (self.cutoff_hz / sample_rate_hz)
-            .max(MIN_CUTOFF_RATIO)
-            .min(MAX_CUTOFF_RATIO);
+        let cutoff_ratio =
+            (self.cutoff_hz / sample_rate_hz).clamp(MIN_CUTOFF_RATIO, MAX_CUTOFF_RATIO);
 
         let filter = butter2(cutoff_ratio).unwrap_or_else(|err| {
             panic!("Failed to construct butter2 filter for ratio {cutoff_ratio}: {err}")
